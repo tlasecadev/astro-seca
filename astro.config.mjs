c959ae92d620c6854841863ea7a0ea21c0c8ffa3
ntlaseca <nate.tlaseca@gmail.com>
2025-12-08 20:08:54 -0700

    add Sanity values to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
-import sanity from '@sanity/astro';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react(), sanity()]
+  integrations: [
+    react(),
+    sanity({
+      projectId: "x9e0p8hx",
+      dataset: "production",
+      useCdn: false,
+    }),
+  ],
 });
