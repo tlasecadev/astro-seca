14c9e4ecef210e4ed52b5477a78848f7aac3d70d
ntlaseca <nate.tlaseca@gmail.com>
2025-12-08 19:54:01 -0700

    add Sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sanity()]
 });
