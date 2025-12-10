f58d86c33273e9a935b69ca684ed1d5063825fcc
ntlaseca <nate.tlaseca@gmail.com>
2025-12-09 21:07:56 -0700

    get waves working

--- src/components/organisms/Waves.jsx
+++ src/components/organisms/Waves.jsx
@@ -3,9 +3,9 @@ import { Plane, MeshWobbleMaterial } from "@react-three/drei";
 
 const Waves = () => {
   return (
-    <div className="fixed -z-10 inset-0 h-full w-full bg-blue">
+    <div className="fixed -z-10 inset-0 bg-blue">
       <Canvas>
-        <pointLight position={[10, 0, 7]} intensity={1.25} />
+        <directionalLight position={[10, 0, 7]} intensity={3} />
         <Plane visible position={[0, 0, 0]} args={[128, 64, 256, 256]} rotation={[0, 0, 0.2]}>
           <MeshWobbleMaterial color="#01469d" attach="material" factor={0.75} speed={0.5} />
         </Plane>
